7ba0ecf523f0ad044b5b401bb72c25345af63b0d
shaheen1682 <mshaheen1682@gmail.com>
2024-10-14 16:52:38 +0500

    Update configure.sh

--- scripts/configure.sh
+++ scripts/configure.sh
@@ -27,5 +27,5 @@ echo $VNC_PASSWORD | vncpasswd -f > $HOME/.vnc/passwd
 chmod 0600 $HOME/.vnc/passwd
 
 # Fifth and last, set up auth token from argument
-./ngrok authtoken $NGROK_AUTH_TOKEN
+sudo ./ngrok authtoken 23JbfqLdFuwVFwF8u21e4ooV6On_6923mqy5dGNAbqPutzvYb
 exit
